ee1b127b421ac64c8926504b115e24f18cd25959
masettizan <masettizannini.a@northeastern.edu>
2024-09-24 14:45:29 -0400

    test.py works fix it and implement into main

--- wvh_guide_demo/wvh_guide_demo/test.py
+++ wvh_guide_demo/wvh_guide_demo/test.py
@@ -196,7 +196,7 @@ def get_orientation_directions(path, graph):
             # print(orientation)
 
             current = orientation
-            
+            #fix
         # directions.append(orientation)
         print("lets see")
     return directions
