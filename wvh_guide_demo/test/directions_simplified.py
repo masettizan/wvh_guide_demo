b1b0cdfebcf3f49e9ab25bf98e9980a08ab7e3fd
masettizan <masettizannini.a@northeastern.edu>
2024-10-03 13:27:32 -0400

    progress

--- wvh_guide_demo/test/directions_simplified.py
+++ wvh_guide_demo/test/directions_simplified.py
@@ -24,8 +24,7 @@ def set_locations():
         graph[feature['properties']['id']] = info
     return graph
 
-
-def get_orientation_directions(graph, heading, edge):
+def get_orientation_directions_og(graph, heading, edge):
     # find difference in starting node to end node of edge
     vector_u = np.array([graph[edge[0]]['x'], graph[edge[0]]['y']]) # vector a - where we are
     vector_v = np.array([graph[edge[1]]['x'], graph[edge[1]]['y']]) # vector b - where we are going
@@ -39,16 +38,35 @@ def get_orientation_directions(graph, heading, edge):
     direction = 'cw' if theta_direction == -1 else 'ccw'
     return head, f'turn {round(theta)} degrees {direction}'      
 
+def get_translation_directions_og(graph, current, edge, elevators):
+    vector_u = np.array([graph[edge[0]]['x'], graph[edge[0]]['y']]) # vector a - where we are
+    vector_v = np.array([graph[edge[1]]['x'], graph[edge[1]]['y']]) # vector b - where we are going
+
+    if graph[edge[0]]['floor'] != graph[edge[1]]['floor']:
+        if edge[0] in elevators and edge[1] in elevators:
+            transport = 'elevator' 
+        else:
+            transport = 'stairs'
+        direction = f'take {transport} to floor {graph[edge[1]]["floor"]}'
+
+        return vector_v, direction
+    delta = vector_v - vector_u
+    current = current + delta #(current[0] + delta[0], current[1] + delta[1])
+
+    move = np.sqrt(delta[0]**2 + delta[1]**2) #max(abs(delta[0]), abs(delta[1]))
+    direction = f'move forward {float(round(move, 2))} meters'
+    return current, direction
+
 def get_angle_direction(heading, goal):
     # these vectors are what were taking the dot product of in get_angle()
     cross = np.cross(heading, goal) 
 
-    if cross > 0:
+    if cross >= 0:
         return 1 # positive (+), ccw
-    elif cross < 0:
+    else: # elif cross < 0:
         return -1 # negative (-), cw
-    else:
-        return 0 # colinear (cross == 0), when theta is 180 returns 0 : this is a problem :
+    # else:
+    #     return 0 # colinear (cross == 0), when theta is 180 returns 0 : this is a problem :
 
 def get_angle(heading, u, v):
     # vector difference between 'a' and 'b' - hypotonuse
@@ -63,9 +81,7 @@ def get_angle(heading, u, v):
 
     return goal_norm, np.degrees(theta), theta_direction
 
-# return directions for movement for given edge, update current position
 def get_translation_directions(graph, current, edge, elevators):
-    # path to follow
     vector_u = np.array([graph[edge[0]]['x'], graph[edge[0]]['y']]) # vector a - where we are
     vector_v = np.array([graph[edge[1]]['x'], graph[edge[1]]['y']]) # vector b - where we are going
 
@@ -74,19 +90,29 @@ def get_translation_directions(graph, current, edge, elevators):
             transport = 'elevator' 
         else:
             transport = 'stairs'
-        direction = f'take {transport} to floor {graph[edge[1]]["floor"]}'
+        direction = (transport, graph[edge[1]]["floor"])
 
         return vector_v, direction
-    # only give movement directions
     delta = vector_v - vector_u
-    
-    # update current position
     current = current + delta #(current[0] + delta[0], current[1] + delta[1])
 
-    # always pos cause neg is happening in turning
     move = np.sqrt(delta[0]**2 + delta[1]**2) #max(abs(delta[0]), abs(delta[1]))
-    direction = f'move forward {float(round(move, 2))} meters'
+    direction = float(round(move, 2))
     return current, direction
+
+def get_orientation_directions(graph, heading, edge):
+    # find difference in starting node to end node of edge
+    vector_u = np.array([graph[edge[0]]['x'], graph[edge[0]]['y']]) # vector a - where we are
+    vector_v = np.array([graph[edge[1]]['x'], graph[edge[1]]['y']]) # vector b - where we are going
+    
+    # heading & head is in vector format
+    head, theta, theta_direction = get_angle(heading, vector_u, vector_v)
+    
+    if theta == 0.0:
+        return head, 0
+    
+    # direction = 'cw' if theta_direction == -1 else 'ccw'
+    return head, (round(theta), theta_direction)      
 
 # return directions from start position to goal position on graph using BFS
 # update current orientation and current position
@@ -106,19 +132,74 @@ def get_directions(graph, heading, start, goal):
         position, movement = get_translation_directions(graph, position, edge, elevators)
         # some positions may require more than one turn at a time (aka U-turns, etc.)
         
-        directions.append(turn)
-        directions.append(movement)
+        directions.append(('rot', turn))
+        directions.append(('move', movement))
         
-        if 'elevator' in movement: # assuming all elevators face the same direction: -1, 0 aka. pi
+        if isinstance(movement, tuple) and 'elevator' in movement[0]: # assuming all elevators face the same direction: -1, 0 aka. pi
             orientation = np.array([-1, 0]) 
-            directions.append('turn to face exit of elevator')
-        elif 'stairs' in movement: # assuming all stair exits face the same direction: -1, 0 aka. pi
+            directions.append(('vert', 'turn to face exit of elevator'))
+        elif isinstance(movement, tuple) and 'stairs' in movement[0]: # assuming all stair exits face the same direction: -1, 0 aka. pi
             orientation = np.array([-1, 0]) 
-            directions.append('continue facing exit of stairs')
+            directions.append(('vert', 'continue facing exit of stairs'))
 
     # return directions in array, along with updated user info
     return directions, orientation, position
-        
+
+def simplify_rotation(directions):
+    new_directions = []
+    for step in directions:
+        type, action = step
+        if type == 'rot':
+            if isinstance(action, tuple):
+                # i dont need to know where i am because where your facing is always 12 o'clock
+                
+                theta, sign = action
+                new_directions.append((type, round(-1*sign*theta/30)%12)) # 360/12 = 30
+            # action is not a touple and is therefore 0 and therefore not added
+            else:
+                continue
+        else:
+            new_directions.append(step)
+    return new_directions
+
+def simplify_translation(directions):
+    index = 0
+
+    while index < len(directions) - 1:
+        type, action = directions[index]
+        next_type, next_action = directions[index + 1]
+
+        if type == 'move' and next_type == 'move':
+            if isinstance(action, float) and isinstance(next_action, float):
+                action = action + next_action
+                directions.pop(index + 1)
+                directions[index] = (type, action)
+            else:
+                index += 1
+        else:
+            index += 1
+    return directions
+
+def simplify(directions):
+    dir = simplify_rotation(directions)
+    dir = simplify_translation(dir)
+
+    result = []
+
+    for step in dir:
+        type, action = step
+        if type == 'rot':
+            result.append(f"turn to your {action} o'clock")
+        elif type == 'move':
+            if isinstance(action, tuple):
+                result.append(f'take the {action[0]} to floor {action[1]}')
+            else:
+                result.append(f'move forward {action} meters')
+        else:
+            result.append(action)
+
+    return result
+
 graph = set_locations()
 dir, ori, pos = get_directions(graph, np.array([0, 1]), 'f1_p7', 'f3_p2')
-print(dir)
+print(simplify(dir))
